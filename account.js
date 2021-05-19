adc6344047774e00b4457dbee02eb1ed73957689
bild96 <bild96@pm.me>
2021-05-19 21:32:38 +0100

    tezospath learning Pathway.

---
+++ account.js
@@ -0,0 +1,25 @@
+let FAUCET_KEY;
+export default FAUCET_KEY = {
+  "mnemonic": [
+    "deputy",
+    "melt",
+    "clarify",
+    "few",
+    "then",
+    "accuse",
+    "rate",
+    "liberty",
+    "garlic",
+    "over",
+    "latin",
+    "trap",
+    "enlist",
+    "gadget",
+    "illness"
+  ],
+  "secret": "fe080e6c3568a677d019ca3ef378c28eb0ed3809",
+  "amount": "22369431924",
+  "pkh": "tz1f2VFXc8QAp1tiCJ6dPCjfQ2YSfT6GrmfC",
+  "password": "B9ih6UmrOT",
+  "email": "nmuamdeq.gvpjsxck@tezos.example.org"
+}
